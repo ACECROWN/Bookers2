d37211812b3df4407047b1febffbd2dd78d35a61
ACECROWN <kenken8tennis@gmail.com>
2021-10-27 16:02:12 +0000

    k

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
   def index
+    @book = Book.new
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_path(@user.id)
+
+    render :index
+
+
+
+
   end
 
   def show
   end
 
   def edit
+  end
+
+   private
+  # ストロングパラメータ
+  def user_params
+    params.require(:user).permit(:title, :body)
   end
 end

6d27d553326e33315ee008e9087897260cafcf20
ACECROWN <kenken8tennis@gmail.com>
2021-11-04 13:21:43 +0000

    課題が終わりました。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,29 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    @user = current_user
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book)
+    else
+     render :index
+    end
+
   end
+
+  def update
+    @user = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+
+    end
+
+  end
+
 
   def index
     @book = Book.new
@@ -17,16 +37,32 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_new = Book.new
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   def edit
     @book = Book.find(params[:id])
+    if current_user != @book.user
+      redirect_to books_path
+    end
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
+    else
+     render :edit
+
+    end
+
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,16 @@ class UsersController < ApplicationController
     @book = Book.new
     @users = User.all
     @user = current_user
+
   end
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to users_path(@user.id)
-
-    render :index
+   if @user.save
+    redirect_to edit_path(@user.id)
+   else
+    render :edit
+   end
   end
 
   def show
@@ -21,13 +23,20 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-
+    if current_user != @user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+
+    end
   end
 
    private
